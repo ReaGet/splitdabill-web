15c866548ac8334bfe3319df40a2a59d40ad5ebb
reaget <reaget.acc@gmail.com>
2025-04-11 00:16:26 +0300

    feat: add update dom translation on lang switch

--- src/js/components/lang-switcher.js
+++ src/js/components/lang-switcher.js
@@ -1,6 +1,8 @@
-import { Dropdown } from "./dropdown";
+import { Dropdown } from './dropdown';
+import _locale from '../shared/locale.json';
 
 class LangSwitcher extends Dropdown {
+  cachedLocales = {};
   constructor(root) {
     super(root);
     this.init();
@@ -16,9 +18,37 @@ class LangSwitcher extends Dropdown {
 
   setNewItemActive(el) {
     super.setNewItemActive(el);
-    this.current.setAttribute('data-value', el.dataset.value);
+    const lang = el.dataset.value;
+    this.current.setAttribute('data-value', lang);
     const newSvg = el.querySelector('svg');
-    this.current.innerHTML = newSvg?.outerHTML || el.dataset.value;
+    this.current.innerHTML = newSvg?.outerHTML || lang;
+    this.updateContent(lang);
+  }
+
+  async updateContent(lang) {
+    const locale = await this.fetchNewLocales(lang);
+    this.applyLocaleToDOM(locale);
+  }
+
+  async fetchNewLocales(lang) {
+    const cached = this.cachedLocales[lang];
+    if (cached) return cached;
+
+    try {
+      return _locale;
+    } catch(e) {
+      console.log('[fetch locale]:', e);
+    }
+  }
+
+  applyLocaleToDOM(locale) {
+    const domItems = document.querySelectorAll('[data-i18]');
+    domItems.forEach(d => {
+      const key = d.dataset.i18,
+        translation = locale[key];
+      if (!translation) return;
+      d.innerText = translation;
+    });
   }
 }
 
